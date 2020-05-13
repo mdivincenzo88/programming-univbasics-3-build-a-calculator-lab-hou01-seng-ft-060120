bb4b404ad126cb4d0f00727a04808ad763ee671c
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-13 22:02:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,18 +3,19 @@ num1 + num2
 end
 
 def subtraction(num1, num2)
-
+num2 - num1
 end
 
 def division(num1, num2)
-
+num2 / num1 
 end
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
 
 def modulo(num1, num2)
+num2 - ((num2 / num1) * num1) 
 
 end
 
